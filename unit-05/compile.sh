b2e4f230a1a7e2d426f3728da593097502ce056d
Tim Richards <richards@cs.umass.edu>
2021-01-06 10:57:17 -0500

    start of unit 5

---
+++ unit-05/compile.sh
@@ -0,0 +1,3 @@
+#! /bin/env bash
+
+gcc -m32 -fno-stack-protector -fno-asynchronous-unwind-tables -fno-exceptions -fno-pic -O0 -S simple.c
---
+++ unit-05/simple.c
@@ -0,0 +1,6 @@
+int xmain(int argc, char const *argv[]) {
+    int a = 2;
+    int b = 3;
+    int c = a + b;
+    return 0;
+}
