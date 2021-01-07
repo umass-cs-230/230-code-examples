a90dc2e950e5ac02e093b4736cf3f08e6611d54d
Tim Richards <richards@cs.umass.edu>
2021-01-07 08:11:46 -0500

    changed names

---
+++ unit-05/app.2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    int a = 2;
+    int b = 3;
+    int c = a + b;
+    printf("%d\n", c);
+    return 0;
+}
--- unit-05/compile.sh
+++ unit-05/compile.sh
@@ -1,3 +1,4 @@
 #! /bin/env bash
 
-gcc -m32 -fno-stack-protector -fno-asynchronous-unwind-tables -fno-exceptions -fno-pic -O0 -S simple.c
+gcc -m32 -fno-stack-protector -fno-asynchronous-unwind-tables -fno-exceptions -fno-pic -O0 -S app.1.c
+gcc -m32 -fno-stack-protector -fno-asynchronous-unwind-tables -fno-exceptions -fno-pic -O0 -S app.2.c
