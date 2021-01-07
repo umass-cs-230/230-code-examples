0eb08dc07b086a80aab1c0d6b9041067586304f5
Tim Richards <richards@cs.umass.edu>
2021-01-07 08:14:21 -0500

    compiles to binaries

--- unit-05/compile.sh
+++ unit-05/compile.sh
@@ -1,4 +1,9 @@
 #! /bin/env bash
 
+# App Example 1
 gcc -m32 -fno-stack-protector -fno-asynchronous-unwind-tables -fno-exceptions -fno-pic -O0 -S app.1.c
+gcc -m32 -fno-stack-protector -fno-asynchronous-unwind-tables -fno-exceptions -fno-pic -O0 -g app.1.c -o app.1
+
+# App Example 2
 gcc -m32 -fno-stack-protector -fno-asynchronous-unwind-tables -fno-exceptions -fno-pic -O0 -S app.2.c
+gcc -m32 -fno-stack-protector -fno-asynchronous-unwind-tables -fno-exceptions -fno-pic -O0 -g app.2.c -o app.2
