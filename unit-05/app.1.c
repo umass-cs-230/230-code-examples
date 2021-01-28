e2014e8106cf764d6064a52b4706349f9a328722
Tim Richards <richards@cs.umass.edu>
2021-01-28 09:02:50 -0500

    renamed unit-05 to week-05

--- unit-05/app.1.c
+++
@@ -1,6 +0,0 @@
-int main(int argc, char const *argv[]) {
-    int a = 2;
-    int b = 3;
-    int c = a + b;
-    return 0;
-}
--- unit-05/app.2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[]) {
-    int a = 2;
-    int b = 3;
-    int c = a + b;
-    printf("%d\n", c);
-    return 0;
-}
